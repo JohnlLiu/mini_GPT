60e34004717a54f5f63562d412e9dce00d58125a
JohnlLiu <j0hn.l@hotmail.com>
2023-04-19 23:06:33 -0600

    added streamlit app

--- run.py
+++ run.py
@@ -1,6 +1,4 @@
 import torch
-import torch.nn as nn
-from torch.nn import functional as F
 from model import BigramLanguageModel
 from model import encode
 from model import decode
@@ -12,7 +10,17 @@ saved_model.load_state_dict(torch.load('GPT_model.pt'))
 
 new_tokens = 500
 
+
+def chat_bot(input):
+    text = input
+    context = torch.tensor([encode(text)], dtype = torch.long)
+    response = decode(saved_model.generate(context, max_new_tokens = new_tokens)[0].tolist())
+
+    return response
+
+
 #run with input text
+'''
 while True:
     text = input()
     if text == 'quit':
@@ -22,3 +30,4 @@ while True:
     #context = torch.tensor([[1,2,3,4,5],[12,3,23,15,8]])
     print(decode(saved_model.generate(context, max_new_tokens = new_tokens)[0].tolist()))
     print('\n')
+'''
